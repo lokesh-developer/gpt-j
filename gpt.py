2610304947167f23d57dbb09f064eb5b94ec9234
Lokesh Kabra <kabral1999@gmail.com>
2022-03-12 20:26:05 +0530

    Create gpt.py

---
+++ gpt.py
@@ -0,0 +1,7 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
+
+# model = AutoModelForCausalLM.from_pretrained("distilgpt2")
+
+print(tokenizer)
